85197e92be4437e656a87c079b7dae171a978abd
Bram Evert <bram.evert@gmail.com>
2020-03-07 23:40:24 -0800

    Touched up plotting functions

--- modelling/data_utils.py
+++ modelling/data_utils.py
@@ -147,29 +147,34 @@ def df_to_features_labels(dataframe):
     y = dataframe['price']
     return x, y
 
+### PLOTTING ###
 
 from keys import mapbox_access_token
 import plotly.graph_objects as go
 
 # cache the populated coordinates
 
+px.set_mapbox_access_token(mapbox_access_token)
+
 populated_coordinates = np.array([])
 
-def remove_empty_coords (X_test,coordinates,lat_step,long_step):
+def remove_empty_coords (X,lats,longs,lat_step,long_step):
     global populated_coordinates
-    # removes coordinates which have no data
+    # removes coordinates which have less than 5 listings in cell+NN
     if populated_coordinates.any():
         print('Using cached coordinates')
         use_coordinates = np.array(populated_coordinates)
-    else:
-        populated_coordinates = []
-        for lat,long in coordinates:
-            if ((X_test['latitude'].between(lat,lat+lat_step)) & (X_test['longitude'].between(long,long + long_step))).any():
-                populated_coordinates.append([lat,long])
-        use_coordinates = np.array(populated_coordinates)
+    else: 
+        use_coordinates = []
+        for lat in lats:
+            lat_df = X[X['latitude'].between(lat - lat_step,lat+lat_step)]
+            if len(lat_df) > 5:
+                for long in longs:
+                    if np.sum(lat_df['longitude'].between(long - long_step,long + long_step)) > 5:
+                        use_coordinates.append([lat,long])
+        use_coordinates = np.array(use_coordinates)
         populated_coordinates = use_coordinates # set cache
     return use_coordinates
-    
 
 def map_model (X_test,model):
     # model should take X_test as an arugment to it's predict function
@@ -183,17 +188,13 @@ def map_model (X_test,model):
     furnished = X_test['furnished'].mode()[0]
     unit_type = X_test['unit_type'].mode()[0]
     
-    # construct a square grid
-    # throw away any points that are not close to real values
+    title = "Price for a {}sqft, {} bed {}".format(area,bedrooms,unit_type)
+    
     lats, lat_step = np.linspace(X_test['latitude'].min(),X_test['latitude'].max(),num=300,retstep=True)
-    longs, long_step = np.linspace(X_test['longitude'].min(),X_test['longitude'].max(),num=300,retstep=True)
+    longs, long_step = np.linspace(X_test['longitude'].min(),X_test['longitude'].max(),num=600,retstep=True)
     coordinate_list = np.array(np.meshgrid(lats,longs)).T.reshape(-1,2)
     
-    # can this be vectorized?
-    # now, remove the rows that aren't near real data
-    # to do this, set a threshold value for how close we need to find a point
-    # for each point in the dataframe, see if there is a point close enough
-    use_coordinates = remove_empty_coords(X_test,coordinate_list,lat_step,long_step)
+    use_coordinates = remove_empty_coords(X_test,lats,longs,lat_step,long_step)
 
     df = pd.DataFrame(use_coordinates,columns=['latitude','longitude'])
     df.loc[:,'date'] = date
@@ -205,86 +206,60 @@ def map_model (X_test,model):
     df['unit_type'] = pd.Categorical(df['unit_type'])
     df = df[['date', 'latitude', 'longitude', 'area', 'bedrooms', 'pets', 'furnished', 'unit_type']]
     
-    #X_geo = df.to_numpy()
     y_geo = model.predict(df)
-    df['price'] = y_geo
-
-    fig = px.scatter_mapbox(df,lon='longitude',lat='latitude',color='price',width=1000,height=800)
-
-    #fig = go.Figure(go.Scattermapbox(lon=list(map(str,list(df['longitude']))),lat=list(map(str,list(df['latitude'])))))#,marker=go.scattermapbox.Marker(size=14)))
-    #go.scattermapbox.Marker(size=14,symbol='square',color=df['price'])
-    #fig.update_layout(mapbox=dict(accesstoken=mapbox_access_token))
-                  
-    # need to fix view and scatter marker size
-    #fig.data[0].marker = dict(size=10,opacity=0.5,symbol='square')
-    #fig = px.scatter_mapbox(df,lon='longitude',lat='latitude',color='price')
+    df['Price ($)'] = y_geo
+    df['marker_size'] = 8
+    center = dict(lat=49.2623962, lon=-123.115429) # city hall
+    fig = px.scatter_mapbox(df,lon='longitude',lat='latitude',color='Price ($)',width=1200,height=1000,center=center,size='marker_size',
+                            zoom=11,size_max=5,opacity=0.6,title=title)
+    
+#     fig = go.Figure()
+#     fig.add_trace(go.Scattermapbox(lon=df['longitude'],
+#                                    lat=df['latitude'],
+#                                    mode='markers',
+#                                    marker={'size': 15, 'color': df['price']})
+#                                   )
+#    fig.update_layout(mapbox=go.layout.Mapbox(accesstoken=mapbox_access_token,center=center,zoom=12),height=1000,width=1200)   
     return fig
 
-# def norm (series,method="var"):
-#     """
-#     Returns the (pandas) series, normalized by the selected method. 
-#     - Method 'var' sets the centre at the median value and divides by the std.
-#     - Method 'range' sets the centre at the median and the min->-1, max->1
-#     Also returns a function that can be used to return the data to real units
-#     """
-#     assert isinstance(series,pd.Series), "data must be a Pandas series"
-#     if method  == "var":
-#         median = series.median()
-#         std = series.std()
-#         norm_series = (series - median)/std
-#         to_real_units = "lambda x: x*{} + {}".format(std,median)
-#         to_norm_units = "lambda x: (x-{})/{}".format(median,std)
-#     elif method == "range":
-#         data_range = series.max() - series.min()
-#         norm_series = (series - series.min())/(0.5*data_range) - 1
-#         to_real_units = "lambda x: (x+1)*0.5*{} + {}".format(data_range,series.min())
-#         to_norm_units = "lambda x: (x - {})/(0.5*{})".format(series.min(),data_range)
-#     return norm_series, to_real_units, to_norm_units
+def time_evolution (X,model):
+    """
+    Plot the time evolution of prices for the typical apartment
+    """
+    # typical apartment characteristics
+    area = X['area'].median()
+    lat = X['latitude'].median()
+    long = X['longitude'].median()
+    bedrooms = X['bedrooms'].mode()[0]
+    pets = X['pets'].mode()[0]
+    furnished = X['furnished'].mode()[0]
+    unit_type = X['unit_type'].mode()[0]
     
+    title = "Price for a {}sqft, {} bed {}".format(area,bedrooms,unit_type)
     
-
-# def normalize_dataset (df,return_converters=False):
-#     """
-#     Converts the dataframe into a format useful for fitting models
-#     - returns a dataframe
-#     - return_converters will return a second value which is a dictionary of functions that can convert back to real units
-#     """
-#     assert all(col in df.columns.tolist() for col in  ['date', 'latitude','longitude','area','bedrooms','unit_type','pets','furnished','price'])
+    dates = np.linspace(X['date'].min(),X['date'].max(),num=24)
     
-#     norm_df = pd.DataFrame()
+    lats, lat_step = np.linspace(X_test['latitude'].min(),X_test['latitude'].max(),num=300,retstep=True)
+    longs, long_step = np.linspace(X_test['longitude'].min(),X_test['longitude'].max(),num=600,retstep=True)
+    coordinate_list = np.array(np.meshgrid(lats,longs)).T.reshape(-1,2)
     
-#     norm_df['date'] = df['date'].astype(int)/1e9
-#     norm_df['date'], real_date, norm_date = norm(norm_df['date'],method="range")
-#     norm_df['latitude'], real_lat, norm_lat = norm(df['latitude'])
-#     norm_df['longitude'], real_long, norm_long = norm(df['longitude'])
-#     norm_df['area'], real_area, norm_area = norm(df['area'])
-
-#     # code the bedrooms as binary variables
-
-#     norm_df['studio'] = (df['bedrooms']==0.0).astype(int)
-#     norm_df['1_bed'] = (df['bedrooms']==1.0).astype(int)
-#     norm_df['2_bed'] = (df['bedrooms']==2.0).astype(int)
-#     norm_df['3_bed'] = (df['bedrooms']==3.0).astype(int)
-#     norm_df['4_bed'] = (df['bedrooms']==4.0).astype(int)
-#     norm_df['5_bed'] = (df['bedrooms']==5.0).astype(int)
-#     norm_df['6_bed'] = (df['bedrooms']==6.0).astype(int)
-#     norm_df['mansion'] = (df['bedrooms']>=7.0).astype(int)
-
-#     # code the unit type as binary variables
-
-#     norm_df['apartment'] = (df['unit_type']=="apartment").astype(int)
-#     norm_df['condo'] = (df['unit_type']=="condo").astype(int)
-#     norm_df['house'] = (df['unit_type']=="house").astype(int)
-#     norm_df['townhouse'] = (df['unit_type']=="townhouse").astype(int)
-
-#     norm_df['pets'] = df['pets']
-#     norm_df['furnished'] = df['furnished']
-
-#     norm_df['price'], real_price, norm_price = norm(df['price'])
-
-#     conversion_functions = {'to_date': real_date, 'to_latitude': real_lat, 'to_longitude': real_long, 'to_area': real_area, 'to_price': real_price,
-#                             'from_date': norm_date, 'from_latitude': norm_lat, 'from_longitude': norm_long, 'from_area': norm_area, 'from_price': norm_price}
-#     if return_converters:
-#         return norm_df, conversion_functions
-#     else:
-#         return norm_df
+    use_coordinates = remove_empty_coords(X_test,lats,longs,lat_step,long_step)
+    
+    prices = []
+    
+    for d in dates:
+        df = pd.DataFrame(use_coordinates,columns=['latitude','longitude'])
+        df.loc[:,'date'] = d
+        df.loc[:,'area'] = area
+        df.loc[:,'bedrooms'] = bedrooms
+        df.loc[:,'pets'] = pets
+        df.loc[:,'furnished'] = furnished
+        df.loc[:,'unit_type'] = unit_type
+        df['unit_type'] = pd.Categorical(df['unit_type'])
+        df = df[['date', 'latitude', 'longitude', 'area', 'bedrooms', 'pets', 'furnished', 'unit_type']]
+        price = model.predict(df).mean()
+        prices.append(price)
+        
+    plot_df = pd.DataFrame({'Date': dates, 'Price ($)': prices})
+    plot_df['Date'] = pd.to_datetime(plot_df['Date'],unit='s')
+    return px.scatter(plot_df,x='Date',y='Price ($)',trendline='ols',title=titleheight=1000,width=1200)
--- readme.md
+++ readme.md
@@ -1,16 +1,3 @@
 # Craigslist Crawler
 
-This is a script that monitors the rss feed of craigslist to scrape rental apartment listings for Vancouver. It stores the listing in a database and extracts key characteristics such as area, location and ammenities. This allows for trend analysis, geographical modelling and determining the market value of apartment characteristics (eg. allowing pets).
-
-It also includes scripts to plot the data and publish to a blog.
-
-## To do:
-
-1. Add additional cities (Toronto, Montreal, Portland)
-2. Create a predictive model for rents based on observable characteristics
-3. Disassoicate the underlying rent trend from changes in observable characteristics
-4. Measure the effects of policy changes
-
-## Dependencies
-* Feedparser
-* ...
+This is a script that monitors the rss feed of craigslist to scrape rental apartment listings for Vancouver. It stores the listings in a database and extracts key characteristics such as area, location and ammenities. This allows for trend analysis, geographical modelling and determining the market value of apartment characteristics.
